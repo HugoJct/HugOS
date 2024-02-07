73f89d105de9a7d44c66c922ba1ca0fb08687f6e
Bobybyk <matt.lefranc@gmail.com>
2024-02-07 16:50:01 +0100

    registers comments

--- src/lgdt.s
+++ src/lgdt.s
@@ -12,12 +12,12 @@ load_gdt:
 ; Loads the segments registers with the offsets for segments within GDT
 ; @return void
 gdt_init_reg:
-	mov ax, 0x10
+	mov ax, 0x10 ; ax is the accumulator register for arithmetic et logical operations
 	mov ds, ax ; 0x10 - an offset into GDT for the third (kernel data segment) record.
-	mov ss, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
+	mov ss, ax ; ss is the stack segment register, store return addresses, local variables...
+	mov es, ax ; es is the extra segment register, point on multi-segment program data
+	mov fs, ax ; general purpose segment register
+	mov gs, ax ; general purpose segment register
 
 	jmp 0x08:flush_cs	; "far jump": jump to flush_cs and set cs to 0x08 (jump does nothing here but cs is set)
 flush_cs:
